ab39b33d9bc002ea4c077efa58f92d92e715fa4a
Luis Bustos <luisbustos500@gmail.com>
2022-10-12 15:48:51 -0600

    if/else exercise

--- exercises/app.js
+++ exercises/app.js
@@ -20,3 +20,29 @@ if(allowedAge >= 18) {
 } else {
     console.log("Access denied")
 }
+
+
+let num1 = 122;
+
+if(num1>0){
+    if(num1>=100){
+        console.log("positive & greater than 100")
+    } else{
+        console.log("positive but less than 100")
+    }
+}else {
+    console.log("negative")
+}
+
+
+let marks=91;
+    if(marks>=90)
+    console.log("Grade A");
+    else if(marks>=80 && marks<90)
+    console.log("Grade B");
+    else if(marks>=70 && marks<80)
+    console.log("Grade C");
+    else if(marks>=55 && marks<70)
+    console.log("Grade D");
+    else
+    console.log("Grade F");
