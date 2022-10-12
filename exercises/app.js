c4d1f7b3880301a1d1180e700c041894c675d351
Luis Bustos <luisbustos500@gmail.com>
2022-10-12 14:53:03 -0600

    added if/else statements

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,22 @@
-console.log(hellp world);
+console.log("hello world");
+
+let num = 3;
+
+if (num>0){
+    console.log("positive");
+}
+else if (num<0){
+    console.log("negative");
+}
+else {
+    console.log("Neither")
+}
+
+
+const allowedAge = 12;
+
+if(allowedAge >= 18) {
+    console.log("Access granted")
+} else {
+    console.log("Access denied")
+}
