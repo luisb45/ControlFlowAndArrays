5d57fc269031b2b9ceb1a251d402e5e1f356eeee
Luis Bustos <luisbustos500@gmail.com>
2022-10-13 14:20:32 -0600

    functions exercise

---
+++ exercises/functions/functions.js
@@ -0,0 +1,14 @@
+console.log("Functions");
+
+//function declaration
+function one() {
+    return 'one'
+}
+
+one(); //calling a function
+
+//function expression
+const two = () => {
+    return 2;
+}
+const shortenedTwo = () => 2;
